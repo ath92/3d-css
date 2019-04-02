ad29ed1d9fe67bf46559991e18a64ec93280b5fd
Tom Hutman <tom@ath92.nl>
2019-04-02 23:00:35 +0200

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,131 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div :style="appPerspective" 
+       id="app" 
+       tabindex="0"
+       @click="requestPointerLock" 
+       @mousemove="updateDirection" 
+       @keydown="onKeydown"
+       @keyup="onKeyup">
+    <div :style="translate" class="scene">
+      <div :style="rotate" class="content">
+        <template v-for="x in 3">
+          <cube v-for="y in 3" :position="{ x: x, y: y, z: 0 }" />
+          <cube :position="{ x: 1, y: 3, z: 1 }" />
+          <cube :position="{ x: 1, y: 3, z: 2 }" />
+        </template>
+      </div>
+    </div>
+    <div class="position">
+      {{ direction.x }} - {{ direction.y }} - {{ direction.z }}
+      <div>{{ position.x }} - {{ position.y }} - {{ position.z }}</div>
+      <div> {{ yAngle }}</div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Cube from './components/Cube.vue';
+import Vec3 from './Vec3';
+
+const speed = 30;
+const up = new Vec3(0, -1, 0);
+const left = forward => forward.rotateAroundAxis(up, 0.5 * Math.PI);
+const perspective = 500;
 
 export default {
-  name: 'app',
   components: {
-    HelloWorld
+    Cube
+  },
+  data: () => ({
+    direction: new Vec3(0, 0, -1),
+    position: new Vec3(0, 0, 0),
+    directionKeys: {
+      front: false,
+      back: false,
+      left: false,
+      right: false,
+      jump: false,
+    }
+  }),
+  computed: {
+    yAngle() {
+      return this.direction.zAngle() * 180 / Math.PI - 180;
+    },
+    rotate() {
+      const sign = this.direction.x > 0 ? -1 : 1;
+      const rotate = `rotateY(${sign * this.yAngle}deg)`
+
+      return { 
+        transform: rotate,
+        transformOrigin: `calc(50% + ${this.position.x}px)
+                          calc(50% + ${this.position.y}px) 
+                          calc(${this.position.z}px + ${perspective}px)`,
+      };
+    },
+    translate() {
+      return { transform: `translate3d(${-this.position.x}px, ${-this.position.y}px, ${-this.position.z}px)` };
+    },
+    appPerspective() {
+      return { perspective: `${perspective}px` };
+    }
+  },
+  methods: {
+    tick() {
+      requestAnimationFrame(this.tick);
+      if (this.directionKeys.jump) {
+        this.position = this.position.add(new Vec3(0, -30, 0));
+        this.directionKeys.jump = false;
+      }
+      if (this.directionKeys.front) {
+        this.position = this.position.add(this.direction.multiplyScalar(speed));
+      } 
+      if(this.directionKeys.back) {
+        this.position = this.position.add(this.direction.multiplyScalar(-speed));
+      }
+      if(this.directionKeys.left) {
+        this.position = this.position.add(left(this.direction).multiplyScalar(speed));
+      }
+      if(this.directionKeys.right) {
+        this.position = this.position.add(left(this.direction).multiplyScalar(-speed));
+      }
+      if (this.position.y < 0) {
+        this.position = this.position.add(new Vec3(0, 0.1, 0));
+      }
+    },
+    onKeydown({ key }) {
+      if (key === 'w') {
+        this.directionKeys.front = true;
+      } else if(key === 's') {
+        this.directionKeys.back = true;
+      } else if(key === 'a') {
+        this.directionKeys.left = true;
+      } else if(key === 'd') {
+        this.directionKeys.right = true;
+      } else if(key === ' ') {
+        console.log('jhbasdhbasjhdba');
+        this.directionKeys.jump = true;
+      }
+    },
+    onKeyup({ key }) {
+      if (key === 'w') {
+        this.directionKeys.front = false;
+      } else if(key === 's') {
+        this.directionKeys.back = false;
+      } else if(key === 'a') {
+        this.directionKeys.left = false;
+      } else if(key === 'd') {
+        this.directionKeys.right = false;
+      }
+    },
+    requestPointerLock() {
+      this.$el.requestPointerLock();
+    },
+    updateDirection(e) {
+      this.direction = this.direction.rotateAroundAxis(up, -e.movementX / 100);
+    }
+  },
+  mounted() {
+    this.tick();
   }
 }
 </script>
@@ -23,6 +137,17 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  overflow: hidden;
+  width: 100vw;
+  height: 100vh;
+}
+
+.scene, .content {
+  transform-style: preserve-3d;
+}
+
+.position {
+  position: relative;
+  z-index: 1000;
 }
 </style>
---
+++ src/components/Cube.vue
@@ -0,0 +1,81 @@
+<template>
+  <div :style="transform" class="cube">
+    <div class="cube__face cube__face--top"></div>
+    <div class="cube__face cube__face--right"></div>
+    <div class="cube__face cube__face--bottom"></div>
+    <div class="cube__face cube__face--left"></div>
+    <div class="cube__face cube__face--front"></div>
+    <div class="cube__face cube__face--back"></div>
+  </div>
+</template>
+
+<script>
+import Vec3 from '../Vec3';
+
+export default {
+  name: 'HelloWorld',
+  props: {
+    position: {
+      type: Object,
+      default: () => new Vec3(0, 0, 0), 
+    },
+  },
+  data: () => ({
+  }),
+  computed: {
+    transform() {
+      const { x, y, z } = this.position;
+      return { transform: `translate3d(${x * 10}vw, ${(y) * 10}vw, ${z * 10}vw)` };
+    },
+  },
+}
+</script>
+
+<style scoped lang="scss">
+
+.cube {
+  width: 10vw;
+  height: 10vw;
+  position: absolute;
+  transform-style: preserve-3d;
+}
+
+.cube__face {
+  backface-visibility: hidden;
+  height: 10vw;
+  position: absolute;
+  transition: .2s ease-out;
+  width: 10vw;
+  border: 5px solid black;
+  &:hover {
+    background-color: rgba(0, 0, 255, .6);
+    transform: rotateY(0deg);
+  }
+}
+
+.cube__face--front  { 
+  background: rgba(255, 0, 0, 1);
+  transform: rotateY(  0deg) translateZ(5vw); 
+}
+.cube__face--right  { 
+  background: rgba(255, 255, 0, 1);
+  transform: rotateY( 90deg) translateZ(5vw);
+}
+.cube__face--back   {
+  background: rgba(255, 0, 255, 1);
+  transform: rotateY(180deg) translateZ(5vw); 
+}
+.cube__face--left   { 
+  background: rgba(0, 255, 255, 1);
+  transform: rotateY(-90deg) translateZ(5vw); 
+}
+.cube__face--top    { 
+  background: rgba(0, 0, 255, 1);
+  transform: rotateX( 90deg) translateZ(5vw); 
+}
+.cube__face--bottom { 
+  background: rgba(0, 255, 0, 1);
+  transform: rotateX(-90deg) translateZ(5vw); 
+}
+
+</style>
